4c0f7278aae8b28b93b3a6ef9fe93c3ddd5ff4e9
Yash Kumar <yashbro1947@gmail.com>
2025-04-11 09:58:02 +0530

    d

---
+++ blogging/python.py
@@ -0,0 +1,17 @@
+import random
+
+quotes = [
+    "❝ Those who break the rules are scum, but those who abandon their friends are worse than scum. ❞ – Obito Uchiha",
+    "❝ I don’t pretend to understand other people. ❞ – Ayanokoji Kiyotaka",
+    "❝ Hard work is worthless for those that don’t believe in themselves. ❞ – Naruto Uzumaki",
+    "❝ Power comes in response to a need, not a desire. You have to create that need. ❞ – Goku",
+    "❝ A lesson you learned from pain is not easily forgotten. ❞ – Holo (Spice and Wolf)",
+    "❝ Fear is not evil. It tells you what your weakness is. ❞ – Gildarts (Fairy Tail)",
+    "❝ Whether a fish lives in a clear stream or a water ditch, so long as it continues swimming forward, it will grow up beautifully. ❞ – Koro-sensei",
+    "❝ If you want to know who you are, you have to look at your real self and acknowledge what you see. ❞ – Yoruichi (Bleach)"
+]
+
+quote = random.choice(quotes)
+print("\n🧠 Your Daily Anime Wisdom 💬:\n")
+print(quote)
+print("\n🔥 Keep grinding, future legend!\n")
